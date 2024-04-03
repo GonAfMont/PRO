bb47c6a6263bc70a4f9cbb07e2c7aa0e2886f65d
GonAfMont <157701843+GonAfMont@users.noreply.github.com>
2024-04-03 13:38:44 +0100

    Modificación

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+import random
 
 print("Hola Mundo!\n") 
+
+# Modificación del código
+x = int(input("Introduce un numero del 1 al 6:\n"))
+if x == random.randint(1,6):
+    print("¡Tu ganas!")
+else:
+    print("Mala suerte.")
